6433077b330029e5ee71ce5e108d76e1760ef089
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-17 23:27:17 -0400

    form skeleton

--- madlibs.php
+++ madlibs.php
@@ -8,6 +8,17 @@
     <link rel="stylesheet" type="text/css" media="screen" href="main.css">
 </head>
 <body>
+
+<form action="madlibs.php" method="get">
+    Color: <input type="text" name="color"> <br>
+    Plural noun: <input type="text" name="pluralNoun"> <br>
+    Celebrity: <input type="text" name="color"> <br>
+    <input type="submit">
+</form>
+<br><br>
+
+<?php ?>
+
     
 </body>
 </html>
